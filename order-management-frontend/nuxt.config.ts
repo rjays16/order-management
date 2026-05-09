ea734457488fdba0849b06894fb7202ab36b84b6
allan81 <rjaycondiman81@gmail.com>
2026-05-10 02:00:58 +0800

    Adding runtimeConfig

--- order-management-frontend/nuxt.config.ts
+++ order-management-frontend/nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiBase: '',
+    },
+  },
 })
